a2c7a4e9bfe3b824d486e219ea6632ae5221d823
Keegan Leitz <kjleitz@gmail.com>
2022-01-09 12:43:37 -0500

    add bootstrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,11 @@
+import initBootstrap from '@/init/initBootstrap';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+initBootstrap();
 
 Vue.config.productionTip = false;
 
