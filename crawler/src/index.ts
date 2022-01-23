054e6948a315aa16c0f9f1e6d494a1b291a1cedc
Keegan Leitz <kjleitz@gmail.com>
2022-01-23 12:00:34 -0500

    crawler listens for redis pubsub to initiate crawling

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -1,22 +1,36 @@
+import { createClient } from "redis";
 import Crawler from "@/Crawler";
 
-// import { chromium } from "playwright";
+const {
+  SEARCHER_BASE_URL = "http://localhost:8000",
+  NODE_ENV = "development",
+} = process.env;
 
-const crawler = new Crawler("http://localhost:8080");
-// crawler.testo();
-crawler.start();
+const redisActionKey = (key: string): string => `birdybop:${NODE_ENV}:action:${key}`;
 
+const redis = createClient();
+const pubsub = redis.duplicate();
 
-// chromium.launch().then((browser) => {
-//   browser.newPage().then((page) => {
-//     page.goto("http://localhost:8080").then(() => {
-//       page.innerHTML("body").then((doc) => {
-//         console.log(doc);
-//         page.close().then(() => {
-//           console.log("closing");
-//           browser.close();
-//         });
-//       });
-//     });
-//   });
-// });
+redis.on("error", console.error);
+pubsub.on("error", console.error);
+
+const queuedSubscribe = (key: string, callback: (value: string) => void): void => {
+  pubsub.subscribe(redisActionKey(key), (queue) => {
+    redis.lPop(queue).then((value) => {
+      // If the value is null/undefined that means it's been picked up by
+      // another crawler process, so we can bail.
+      if (value !== null && typeof value !== "undefined") callback(value);
+    });
+  });
+};
+
+Promise.all([
+  redis.connect(),
+  pubsub.connect(),
+]).then(() => {
+  queuedSubscribe("source:crawl", (baseUrl) => {
+    console.log("heard!", baseUrl)
+    const crawler = new Crawler({ baseUrl, searcherBaseUrl: SEARCHER_BASE_URL});
+    crawler.start();
+  });
+});
